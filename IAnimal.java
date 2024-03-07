dffb2e01733c4785fd0d906a271881316df3e0fe
Trustfulracer <156245777+Trustfulracer@users.noreply.github.com>
2024-03-07 15:03:27 -0500

    IAnimal.java finished

--- IAnimal.java
+++ IAnimal.java
@@ -17,28 +17,51 @@
 public interface IAnimal
 {
     /**
+     * returns the value of commonName
      *
+     * @return commonName, a String
+     *
+     * @pre commonName != NULL
+     *
+     * @post
+     *      getCommonName = commonName AND genus = #genus AND species = #species
      */
     public String getCommonName();
 
     /**
+     * returns the value of genus
      *
+     * @return genus, a String
      *
+     * @pre genus != NULL
      *
+     * @post
+     *      getGenus = genus AND commonName = #commonName AND species = #species
      */
     public String getGenus();
 
     /**
+     * returns the value of species
      *
+     * @return species, a String
      *
+     * @pre species != NULL
      *
+     * @post
+     * getSpecies = species AND genus = #genus AND commonName = #commonName
      */
     public String getSpecies();
 
     /**
+     * returns the description of the animal
      *
+     * @return a combination of strings as a single String
      *
+     * @pre string != NULL
      *
+     * @post
+     * getDescription = getCommonName() + properGenusFormatting + getSpecies()
+     * commonName = #commonName AND genus = #genus AND species = #species
      */
     public default String getDescription(){
         String properGenusFormatting = getGenus().substring(0,1).toUpperCase() + getGenus().substring(1).toLowerCase();

862f56a525312feda5ad8707b2c7aee8c95b4746
RNSpenc <146198863+RNSpenc@users.noreply.github.com>
2024-03-07 14:48:50 -0500

    added back imammal changes

--- IMammal.java
+++ IMammal.java
@@ -1,16 +1,16 @@
 /**
  *
  *
- * @defines
+ * @defines the behaviors specific to mammals, extending the general animal behavior
  *
- * @constraints
+ * @constraints legCount >= 0
  *
- * @initialization_ensures
+ * @initialization_ensures that legcount is initiialized to 0
  */
 public interface IMammal extends IAnimal
 {
     /**
-     *
+     * @return the number of legs a certain mammal may have
      */
     public int getLegCount();
 }

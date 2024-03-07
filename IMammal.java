734a7d245e654767a880933ceb280cb2928459d5
akstric <156359009+akstric@users.noreply.github.com>
2024-03-07 14:37:07 -0500

    Implemented IMammal interface specifications
    Interface implementation for IMammal

--- IMammal.java
+++ IMammal.java
@@ -1,16 +1,17 @@
 /**
  *
  *
- * @defines
+ * @defines the behaviors specific to mammals, extending the general animal behavior
  *
- * @constraints
+ * @constraints legCount >= 0
  *
- * @initialization_ensures
+ * @initialization_ensures that legcount is initiialized to 0
  */
+
 public interface IMammal extends IAnimal
 {
     /**
-     *
+     * @return the number of legs a certain mammal may have
      */
     public int getLegCount();
 }

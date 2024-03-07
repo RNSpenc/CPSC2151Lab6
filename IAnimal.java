852bb47a0b1b79bd1288e5b40b015902c965065e
Trustfulracer <156245777+Trustfulracer@users.noreply.github.com>
2024-03-07 14:41:45 -0500

    Interface specification for IAnimal

--- IAnimal.java
+++ IAnimal.java
@@ -1,10 +1,18 @@
 /**
  *
- * @defines
+ * @defines genus: aGenus
+ *          species: aSpecies
+ *          commonName: aName
+ *          if Fish(
+ *          fin: finCount
+ *          )
+ *          if Mammal(
+ *          leg: legCount
+ *          )
  *
- * @constraints
+ * @constraints The strings cannot be empty and fin and leg cannot be less than 0
  *
- * @initialization_ensures
+ * @initialization_ensures an animal will have a name a species and a genus then if fish it will have a fin count and if it is a Mammal it will have a leg count
  */
 public interface IAnimal
 {
